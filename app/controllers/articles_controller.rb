530ed480100f1fcd07d282f4d52ef0ebbd2c06cc
wathika <vcwathika@gmail.com>
2015-10-05 18:43:55 +0300

    delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,13 @@ class ArticlesController < ApplicationController
     @article.save
 
     redirect_to article_path(@article)
+  end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+  redirect_to action: "index"
   end
 
 end
